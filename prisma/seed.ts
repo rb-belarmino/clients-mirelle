421bd7f8e0572c31f2304e341a5b7117c768de7a
Rodrigo Belarmino <rb.belarmino@gmail.com>
2025-07-07 09:58:31 -0300

    feat: clear clientData array in seed.ts to reset seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,24 +2,7 @@ import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const clientData: Prisma.ClientCreateInput[] = [
-  {
-    nome: 'Mirelle Amanda',
-    cpf: '12345678901',
-    senha_gov: 'senha123',
-    data_nascimento: new Date('04/06/1991'),
-    cnpj: '12345678000195',
-    cod_simples: '123456789'
-  },
-  {
-    nome: 'Rodrigo Belarmino',
-    cpf: '98765432100',
-    senha_gov: 'senha456',
-    data_nascimento: new Date('12/25/1985'),
-    cnpj: '98765432000198',
-    cod_simples: '987654321'
-  }
-]
+const clientData: Prisma.ClientCreateInput[] = []
 export async function main() {
   console.log('Start seeding ...')
 
